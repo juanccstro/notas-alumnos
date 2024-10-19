acf0dbcec75ef1ad6006589edebb419fc4497115
juanccstro <castro.pazo.jc@gmail.com>
2024-10-19 11:47:11 +0200

    Añadimos elementos al view

--- ud2-proyecto/views/calculo-notas.juan.view.php
+++ ud2-proyecto/views/calculo-notas.juan.view.php
@@ -7,17 +7,62 @@
 <!-- Content Row -->
 
 <div class="row">
+    <div class="col-12">
+
+    </div>
 
     <div class="col-12">
         <div class="card shadow mb-4">
+            <div class="card-body">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>
+                            Módulo
+                        </th>
+                        <th>
+                            Nota media
+                        </th>
+                        <th>
+                            Aprovados
+                        </th>
+                        <th>
+                            Suspensos
+                        </th>
+                        <th>
+                            Máximo
+                        </th>
+                        <th>
+                            Mínimo
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
             <div
-                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                <h6 class="m-0 font-weight-bold text-primary"><?php echo $data['div_titulo']; ?></h6>
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">Datos de las asignaturas</h6>
             </div>
             <div class="card-body">
-                <?php
-                echo $data['texto'];
-                ?>
+                <form method="post">
+                    <label for="texto">Datos:</label>
+                    <textarea class="form-control" name="json" id="json"
+                              placeholder="Introduce aquí el JSON"></textarea>
+                    <p class="alert-danger-small"></p>
+                    <div>
+                        <input type="submit" value="Analizar Datos" name="enviar"/>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
