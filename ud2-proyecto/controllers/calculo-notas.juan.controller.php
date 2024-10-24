4bfe19589444b14ee40e76c55217be076088edbf
juanccstro <castro.pazo.jc@gmail.com>
2024-10-24 17:42:39 +0200

    Creo función comprobarErrores()

--- ud2-proyecto/controllers/calculo-notas.juan.controller.php
+++ ud2-proyecto/controllers/calculo-notas.juan.controller.php
@@ -2,9 +2,44 @@
 /*
  * Aquí hacemos los ejercicios y rellenamos el array de datos.
  */
-$data['titulo'] = "Ejercicio ud3";
-$data["div_titulo"] = "Cálculos de notas";
-$data['texto'] = "Preparado para ejercicio de cálculo de notas";
+
+/*
+* Comprobar errores del formulario (campos vacíos, incompletos o incorrectos
+*/
+function comprobarErroresForm(string $texto): array
+{
+    $errores = [];
+    if (empty($texto)) {
+        $errores['texto'][] = 'El campo es obligatorio';
+    } else {
+        $datos = json_decode($texto);
+        if (is_null($datos)) {
+            $errores['texto'][] = 'Debes introducir un JSON valido';
+        } else {
+            if (!is_array($datos)) {
+                $errores['texto'][] = 'Debes introducir un array dentro del JSON';
+            } else {
+                foreach ($datos as $materia => $alumnos) {
+                    if (!is_string($materia) || mb_strlen($materia) == 0)
+                        $errores['texto'][] = "Debes introducir una asignatura valida, ERROR: '$materia'";
+                }
+                if (!is_array($alumnos)) {
+                    $errores['texto'][] = "El JSON debe ser un array de alumnos, ERROR: '$materia'";
+                } else {
+                    foreach ($alumnos as $alumno => $nota) {
+                        if (!is_string($alumno) || mb_strlen(trim($alumno)) == 0) {
+                            $errores['texto'][] = "Debes introducir un alumno válido, ERROR: '$alumno' de la materia '$materia'";
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    return $errores;
+}
+
+
 
 /*
  * Llamamos a las vistas
