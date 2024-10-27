30ac827ade08786ca26d2a096cf6b1a48d03cbe6
juanccstro <castro.pazo.jc@gmail.com>
2024-10-27 10:24:19 +0100

    Comprobar datos introducidos y llenar variables con ellos

--- ud2-proyecto/controllers/calculo-notas.juan.controller.php
+++ ud2-proyecto/controllers/calculo-notas.juan.controller.php
@@ -2,6 +2,66 @@
 /*
  * Aquí hacemos los ejercicios y rellenamos el array de datos.
  */
+declare(strict_types=1);
+
+$data = [];
+
+if (!empty($_POST)) {
+    $data['errores'] = comprobarErroresForm($_POST['texto']);
+    $data['input']['texto'] = filter_var($_POST['texto'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if (empty($data['errores'])) {
+        $datos = ($_POST['texto']);
+        $alumnosSusp = [];
+        $resultado = [];
+
+        // Recorro el array de materias y dentro de ese el de los alumnos que contiene poniendo nombre a esos valores
+        foreach ($datos as $materia => $alumnos) {
+            $datosMateria = [];
+
+            $suspensos = 0;
+            $aprobados = 0;
+            $sumaNotas = 0;
+            $min = [];
+            $min['alumno'] = '';
+            $min['nota'] = -1;
+            $max = [];
+            $max['alumno'] = '';
+            $max['nota'] = 10;
+
+
+            //Recorre array de notas y contabiliza los suspensos y aprobados
+            foreach ($alumnos as $alumno => $nota) {
+                $alumnosSusp[$alumno] = 0;
+                $sumaNotas += $nota;
+                if ($nota < 5) {
+                    $suspensos++;
+                    $alumnosSusp[$alumno]++;
+                } else
+                    $aprobados++;
+            }
+            if ($min['nota'] < $nota)
+                $alumnoMin['alumno'] = $alumno;
+            $notaMin['nota'] = $nota;
+        }
+        if ($max['nota'] < $nota)
+            $alumnoMax['alumno'] = $alumno;
+        $notaMax['nota'] = $nota;
+    }
+    $datosMateria['notaMedia'] = $sumaNotas / count($alumnos);
+    $datosMateria['suspensos'] = $suspensos;
+    $datosMateria['aprobados'] = $aprobados;
+    if (!empty ($alumnos)) {
+        $datosMateria['notaMax'] = $notaMax;
+        $datosMateria['notaMin'] = $notaMin;
+    } else {
+        $datosMateria['max'] = ['alumno' => '', 'nota' => ''];
+        $datosMateria['min'] = ['alumno' => '', 'nota' => ''];
+    }
+    $resultado['materia'] = $datosMateria;
+    $data['resultado'] = $resultado;
+}
+
 
 /*
 * Comprobar errores del formulario (campos vacíos, incompletos o incorrectos
@@ -38,7 +98,6 @@ function comprobarErroresForm(string $texto): array
     }
     return $errores;
 }
-
 
 
 /*
